a73ef10edf317e66bd876c96ec614152bbb5731b
Gabriel Peal <gabriel@tonal.com>
2019-04-11 21:05:55 +0200

    Added auth

--- src/App.js
+++ src/App.js
@@ -1,37 +1,54 @@
 import React, { Component } from 'react';
+import netlifyIdentity from "netlify-identity-widget"
+
 import './App.css';
 class SlackMessage extends Component {
   constructor(props) {
     super(props);
-    this.state = {loading: false, text: null, error: null, success: false};
+    this.state = { loading: false, text: null, error: null, success: false };
   }
   handleText = (e) => {
-    this.setState({text: e.target.value});
+    this.setState({ text: e.target.value });
   };
+
+  generateHeaders = () => {
+    const headers = { "Content-Type": "application/json" };
+    if (netlifyIdentity.currentUser()) {
+      return netlifyIdentity.currentUser().jwt().then((token) => {
+        return { ...headers, Authorization: `Bearer ${token}` };
+      })
+    }
+    return Promise.resolve(headers);
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
-    this.setState({loading: true});
-    fetch('/.netlify/functions/slack', {
-      method: "POST",
-      body: JSON.stringify({
-        text: this.state.text
+    this.setState({ loading: true });
+    this.generateHeaders().then((headers) => {
+      console.log("Headers are", headers)
+      fetch('/.netlify/functions/slack', {
+        method: "POST",
+        headers,
+        body: JSON.stringify({
+          text: this.state.text
+        })
       })
-    })
-    .then(response => {
-      if (!response.ok) {
-        return response.text().then(err => {throw(err)});
-      }
-    })
-    .then(() => this.setState({loading: false, text: null, success: true, error: null}))
-    .catch(err => this.setState({loading: false, success: false, error: err.toString()}))
+        .then(response => {
+          if (!response.ok) {
+            return response.text().then(err => { throw (err) });
+          }
+        })
+        .then(() => this.setState({ loading: false, text: null, success: true, error: null }))
+        .catch(err => this.setState({ loading: false, success: false, error: err.toString() }));
+    });
   }
   render() {
-    const {loading, text, error, success} = this.state;
+    const { loading, text, error, success } = this.state;
     return <form onSubmit={this.handleSubmit}>
       {error && <p><strong>Error sending message: {error}</strong></p>}
       {success && <p><strong>Done! Message sent to Slack</strong></p>}
       <p>
-        <label>Your Message: <br/>
+        <label>Your Message: <br />
           <textarea onChange={this.handleText} value={text}></textarea>
         </label>
       </p>
@@ -42,13 +59,21 @@ class SlackMessage extends Component {
   }
 }
 class App extends Component {
+  componentDidMount() {
+    netlifyIdentity.init();
+  }
+  handleIdentity = (e) => {
+    e.preventDefault();
+    netlifyIdentity.open();
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Slack Messenger</h1>
         </header>
-        <SlackMessage/>
+        <p><a onClick={this.handleIdentity}>User Status</a></p>
+        <SlackMessage />
       </div>
     );
   }
--- src/lambda/slack.js
+++ src/lambda/slack.js
@@ -1,6 +1,11 @@
-const fetch = require("node-fetch");
+import fetch from 'node-fetch';
 const slackURL = process.env.SLACK_WEBHOOK_URL;
 export function handler(event, context, callback) {
+  const claims = context.clientContext && context.clientContext.user;
+  console.log("User", context)
+  if (!claims) {
+    return callback(null, { statusCode: 401, body: "You must be signed in to call this function" });
+  }
   if (event.httpMethod !== "POST") {
     return callback(null, { statusCode: 410, body: "Unsupported Request Method" });
   }
@@ -8,7 +13,12 @@ export function handler(event, context, callback) {
     const payload = JSON.parse(event.body);
     fetch(slackURL, {
       method: "POST",
-      body: JSON.stringify({ text: payload.text })
+      body: JSON.stringify({
+        text: payload.text,
+        attachments: [
+          { "text": `From ${claims.email}` }
+        ]
+      })
     }).then(() => {
       callback(null, { statusCode: 204 });
     }).catch((e) => {
