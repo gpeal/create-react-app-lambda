940774c1357baddd07fac253b115a3bab22c0bec
Gabriel Peal <gabriel@tonal.com>
2019-04-11 18:10:19 +0200

    Added Slack form

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-class LambdaDemo extends Component {
+class SlackMessage extends Component {
   constructor(props) {
     super(props);
-    this.state = { loading: false, msg: null };
+    this.state = {loading: false, text: null, error: null, success: false};
   }
-
-  handleClick = api => e => {
-    e.preventDefault();
-
-    this.setState({ loading: true });
-    fetch('/.netlify/functions/' + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }));
+  handleText = (e) => {
+    this.setState({text: e.target.value});
   };
-
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({loading: true});
+    fetch('/.netlify/functions/slack', {
+      method: "POST",
+      body: JSON.stringify({
+        text: this.state.text
+      })
+    })
+    .then(response => {
+      if (!response.ok) {
+        return response.text().then(err => {throw(err)});
+      }
+    })
+    .then(() => this.setState({loading: false, text: null, success: true, error: null}))
+    .catch(err => this.setState({loading: false, success: false, error: err.toString()}))
+  }
   render() {
-    const { loading, msg } = this.state;
-
-    return (
+    const {loading, text, error, success} = this.state;
+    return <form onSubmit={this.handleSubmit}>
+      {error && <p><strong>Error sending message: {error}</strong></p>}
+      {success && <p><strong>Done! Message sent to Slack</strong></p>}
       <p>
-        <button onClick={this.handleClick('hello')}>
-          {loading ? 'Loading...' : 'Call Lambda'}
-        </button>
-        <button onClick={this.handleClick('async-chuck-norris')}>
-          {loading ? 'Loading...' : 'Call Async Lambda'}
-        </button>
-        <br />
-        <span>{msg}</span>
+        <label>Your Message: <br/>
+          <textarea onChange={this.handleText} value={text}></textarea>
+        </label>
       </p>
-    );
+      <p>
+        <button type="submit" disabled={loading}>{loading ? "Sending Slack Message..." : "Send a Slack Message"}</button>
+      </p>
+    </form>;
   }
 }
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
+          <h1 className="App-title">Slack Messenger</h1>
         </header>
+        <SlackMessage/>
       </div>
     );
   }
 }
-
 export default App;
--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,11 +1,20 @@
-// show object spread works, i.e. babel works
-const obj = {
-  foo: 'bar'
-};
+const fetch = require("node-fetch");
+const slackURL = process.env.SLACK_WEBHOOK_URL;
 export function handler(event, context, callback) {
-  console.log('queryStringParameters', event.queryStringParameters);
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!', ...obj })
-  });
+  if (event.httpMethod !== "POST") {
+    return callback(null, { statusCode: 410, body: "Unsupported Request Method" });
+  }
+  try {
+    const payload = JSON.parse(event.body);
+    fetch(slackURL, {
+      method: "POST",
+      body: JSON.stringify({ text: payload.text })
+    }).then(() => {
+      callback(null, { statusCode: 204 });
+    }).catch((e) => {
+      callback(null, { statusCode: 500, body: "Internal Server Error: " + e });
+    })
+  } catch (e) {
+    callback(null, { statusCode: 500, body: "Internal Server Error: " + e });
+  }
 }
